c94ed1b4db58b3e7e0f1ff27e7869abe22c0a488
Jonathan Infante <jonathan.infante@revature.com>
2022-11-04 15:51:14 -0400

    go

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ import pandas as pd
 import os
 
 
-EMAIL = os.getenv("EMAIL")
-PWD = os.getenv("PWD")
+EMAIL = ""
+PWD = ""
 
 driver = webdriver.Edge('C:\Program Files (x86)/Microsoft/Edge/Application/msedgedriver')
 driver.get("https://app.revature.com/caliber/home")
+
+initial_batch = "00001230"
 
 
 class topQCScores():
@@ -26,7 +28,9 @@ class topQCScores():
 
     def main(self):
         self.wake_up()
-        self.searching("00001230")
+        self.searching(initial_batch)
+        row_values = [self.names, self.qc_scores, self.trainer_scores, self.trainers]
+        self.associatesToDataFrame(row_values)
 
     def wake_up(self):
         username = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element("id","loginForm:userName-input-id"))
@@ -48,40 +52,59 @@ class topQCScores():
         driver.get("https://app.revature.com/caliber/reports")
 
         driver.switch_to.window(driver.window_handles[0])
-        driver.find_element("id","radioPrevious").click()
+        time.sleep(1)
+        radio_button = driver.find_element("id","radioPrevious").click()
 
-        time.sleep(2)
-        driver.find_element("id", "reportBatchesDropDown").click()
-
-
-
-    def parseBatch(self,batchid):
+    def parseBatch(self, batchid):
+        search_element_path = "/html[1]/body[1]/app-root[1]/div[1]/app-reports-container[1]/div[1]/app-reports-toolbar[1]/div[2]/div[1]/div[2]/button[1]"
+        search_ele = driver.find_element("xpath", search_element_path)
         if self.iter > 0:
             time.sleep(1)
-            search = driver.find_element("xpath", (f"//*[contains(text(), '{batchid}')]"))
+            search_ele.click()
+            input_search_bar_path = "/html[1]/body[1]/app-root[1]/div[1]/app-reports-container[1]/div[1]/app-reports-toolbar[1]/div[2]/div[1]/div[2]/div[1]/div[1]/input[1]"
+            search = driver.find_element("xpath", input_search_bar_path)
+            search.clear()
             search.send_keys(batchid)
+            try:
+                batch = WebDriverWait(driver, timeout=1).until(lambda d: d.find_element("xpath", (f"//*[contains(text(), {batchid})]")))
+                batch.click()
+            except:
+                self.iter += 1
+                new_batch_num = str((int(batchid) + 1))
+                batchid = new_batch_num.zfill(8)
+                print(batchid)
+                self.parseBatch(batchid)
         associate_names = []
         associate_qc_scores = []
         associate_trainer_scores = []
         associate_trainers = []
 
-        WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(By.CSS_SELECTOR, "tbody"))
+        try:
+            tbody = WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(By.CSS_SELECTOR, "tbody"))
+        except:
+            self.iter += 1
+            new_batch_num = str((int(batchid) + 1))
+            batchid = new_batch_num.zfill(8)
+            self.parseBatch(batchid)
+
+        a = ActionChains(driver)
 
         tbody_path = "/html[1]/body[1]/app-root[1]/div[1]/app-reports-container[1]/div[2]/app-qc-scores[1]/div[1]/div[1]/div[1]/div[2]/div[1]/table[1]/tbody[1]"
         associatesRemaining = True
         trainer = driver.find_element("xpath", (f"//*[contains(text(), 'Trainer & QC Scores')]"))
+        a.move_to_element(trainer).perform()
         trainer = trainer.text
 
+
         i = 1
-        a = ActionChains(driver)
         while (associatesRemaining):
             try:
-                l_name = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[1]'))
+                l_name = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[1]'))
                 a.move_to_element(l_name).perform()
                 a.key_down(Keys.DOWN).send_keys("abc").perform()
-                f_name = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[2]'))
-                trainer_score = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[3]/div[1]/div[2]'))
-                qc_score = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[4]/div[1]/div[2]'))
+                f_name = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[2]'))
+                trainer_score = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[3]/div[1]/div[2]'))
+                qc_score = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element("xpath", f'{tbody_path}/tr[{i}]/td[4]/div[1]/div[2]'))
                 associate_names.append(f_name.text + " " + l_name.text)
                 if qc_score == '': qc_score = "0"
                 if trainer_score == '': trainer_score = "0"
@@ -92,6 +115,7 @@ class topQCScores():
                 print("We have no more names to iterate over")
 
             i += 1
+        
 
         self.names += associate_names
         self.qc_scores += associate_qc_scores
@@ -103,16 +127,43 @@ class topQCScores():
         print(associate_trainer_scores)
         print(associate_qc_scores)
         print(associate_trainers)
+        time.sleep(0.5)
+        self.iter += 1
 
-    def searching(self, batchid):
-        try:
-            batch = WebDriverWait(driver, timeout=1).until(lambda d: d.find_element("xpath", (f"//*[contains(text(), {batchid})]")))
+
+    def searching(self, batchid,):
+        input_search_bar_path = "/html[1]/body[1]/app-root[1]/div[1]/app-reports-container[1]/div[1]/app-reports-toolbar[1]/div[2]/div[1]/div[2]/div[1]/div[1]/input[1]"
+
+        try: 
+            driver.find_element("id", "reportBatchesDropDown").click()
+            search = driver.find_element("xpath", input_search_bar_path)
+            search.clear()
+            search.send_keys(batchid)
+            batch = WebDriverWait(driver, timeout=0.5).until(lambda d: d.find_element("xpath", (f"//*[contains(text(), {batchid})]")))
             batch.click()
         except:
-            print("time out")
-    
-        self.parseBatch(batchid)  
+            new_batch_num = str((int(batchid) + 1))
+            batchid = new_batch_num.zfill(8)
+            self.searching(batchid)
+            
+        while int(batchid) < 1600:
+            self.parseBatch(batchid)
+            new_batch_num = str((int(batchid) + 1))
+            batchid = new_batch_num.zfill(8)
 
+    def associatesToDataFrame(self, row_values):
+        new_row_values = []
+        for i in range(len(row_values[0])-1):
+            new_row_values.append([row_values[0][i], row_values[1][i], row_values[2][i], row_values[3][i]])
+
+
+        column_names = ["Name", "QC Score", "Trainer Score", "Trainer/Tech Stack"]
+        df = pd.DataFrame(new_row_values, columns=column_names, dtype=object) #Creating the Data Frame
+
+        self.exportToExcel(df)   
+
+    def exportToExcel(self, df):
+        df = df.to_excel("output/associateReport.xlsx")   
 
 if __name__ == "__main__":
     qc = topQCScores()
